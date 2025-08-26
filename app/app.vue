9773ff04cc9fb6ff76a875b5a8c8815510709783
Avery Howard <avery.d.howard@gmail.com>
2025-08-26 13:14:29 -0400

    init

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen flex justify-center items-center text-6xl text-blue-500 pb-[10vh]">
+    <span>Starter Nuxt 4 TailwindCSS App</span>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+  ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
+  eslint: {
+    config: {
+      standalone: false
+    }
+  },
+})
